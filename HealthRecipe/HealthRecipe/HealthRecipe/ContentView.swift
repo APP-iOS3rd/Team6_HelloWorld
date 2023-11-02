3eb9898d0a91b88f6aa547769a8476ed5c2b9cf3
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-11-02 14:20:46 +0900

    HealthRecipe_Create
    HealthRecipe Project Create

---
+++ HealthRecipe/HealthRecipe/HealthRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthRecipe
+//
+//  Created by Taejun Ha on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
