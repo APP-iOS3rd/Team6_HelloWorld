f29dda44eb0bdb410fc5c0a9d6e01dfab803cd80
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-11-02 17:55:43 +0900

    Revert "HealthDeatil Complete"
    This reverts commit d658337648fd1ba393b9c73dc07f8aacd84c238d.

--- HealthRecipe/HealthRecipe/HealthRecipe/HealthDetail.swift
+++ HealthRecipe/HealthRecipe/HealthRecipe/HealthDetail.swift
@@ -8,34 +8,11 @@
 import SwiftUI
 
 struct HealthDetail: View {
-    
-    let seletedRecipes : Recipe
-    
-    
     var body: some View {
-        Form{
-            Section(header: Text("receipe Detail")){
-                Image(systemName: seletedRecipes.imageName)
-                    .resizable()
-                    .cornerRadius(12.0)
-                    .aspectRatio(contentMode: .fit)
-                    .padding()
-                Text(seletedRecipes.name)
-                    .font(.headline)
-                Text(seletedRecipes.ingredient)
-                    .font(.body)
-                Text(seletedRecipes.cookingOrder)
-                    .font(.body)
-                
-                
-                
-            }
-        }
-        
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    HealthDetail(seletedRecipes: .init(id: "", name: "", ingredient: "", cookingOrder: "", imageName: ""))
+    HealthDetail()
 }
