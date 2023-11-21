02b2fdc84fd5b279901d1e72e26d057872c2ac00
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-11-21 23:31:10 +0900

    [UIKit] - DocumentBrowser

---
+++ Code_Review/Taejun Ha/UIKit/DocumentBrowser_Tutorial/DocumentBrowser_Tutorial/DocumentViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DocumentViewController.swift
+//  DocumentBrowser_Tutorial
+//
+//  Created by Taejun Ha on 11/21/23.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+
+    @IBOutlet weak var documentNameLabel: UILabel!
+    @IBOutlet var documentText: UITextView!
+
+    var document: Document?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        document?.open(completionHandler: { success in
+            if success {
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+                self.documentText.text = self.document?.userText
+            } else {
+                
+            }
+        })
+    }
+
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true)
+    }
+    @IBAction func saveFile(_ sender: Any) {
+        document?.userText = documentText.text
+        
+        if let url = document?.fileURL {
+            document?.save(to: url, for: .forOverwriting, completionHandler: {success in
+                if success {
+                    print("File Save OK!")
+                } else {
+                    print("File Save Failed!")
+                }
+            })
+        } // end if
+    }
+}
