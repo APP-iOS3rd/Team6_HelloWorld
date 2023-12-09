9e4a132d22419bd9ab26ceea7467d462373cfd76
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-12-09 23:15:02 +0900

    [SwiftUI] Chapter23_Multiplatform
    [SwiftUI] Chapter23_Multiplatform

---
+++ Code_Review/Taejun Ha/SwiftUI/Chapter23_Multiplatform/Chapter23_Multiplatform/Chapter23_MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter23_MultiplatformApp.swift
+//  Chapter23_Multiplatform
+//
+//  Created by Taejun Ha on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Chapter23_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
