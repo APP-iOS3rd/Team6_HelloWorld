23dddca6dc7470b0f21aebc9ce58ab7ec8968ca3
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-12-22 18:00:14 +0900

    [SwiftUI] ProgressView
    [SwiftUI] ProgressViewDemo

---
+++ Code_Review/Taejun Ha/SwiftUI/ProgressViewDemo/ProgressViewDemo/ProgressViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewDemoApp.swift
+//  ProgressViewDemo
+//
+//  Created by Taejun Ha on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
