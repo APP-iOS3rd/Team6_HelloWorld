0ab6bf1fe8c19ff02b489f94ee3bd0befafc3eda
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-12-23 23:42:20 +0900

    [SwiftUI] Chart
    [SwiftUI] ChartDemo

---
+++ Code_Review/Taejun Ha/SwiftUI/ChartDemo/ChartDemo/ChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartDemoApp.swift
+//  ChartDemo
+//
+//  Created by Taejun Ha on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
